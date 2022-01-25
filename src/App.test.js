7e361250c911a66e11c20f99a541df5a5cfaddcd
Patricia Navarro <patricia@Patricias-MacBook-Pro.local>
2022-01-25 19:25:05 +0800

    Updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import Aztro from "./App.js";
+import { chooseSign } from "./App.js";
+import {render, screen} from '@testing-library/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const server = setupServer(
+  rest.get("https://aztro.sameerkumar.website", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ description: "lucky day today"}));
+  })
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
+
+test("get data", async () => {
+  const horoscope = await chooseSign;
+  expect(horoscope).toEqual({ description: "lucky day today"})
+  //expect(await screen.findByText("lucky day today")).toBeInTheDocument()
+})
+
